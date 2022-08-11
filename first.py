db4e9f476549fad7276c08a325cbffecba5d315a
jeffin997 <79822558+jeffin997@users.noreply.github.com>
2022-08-11 15:51:12 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,5 @@
+import numpy as np
+x = np.array([12,43,65,76,89,99,76,54])
+print(np.mean(x))
+print(np.median(x))
+print(np.std(x))
---
+++ visual.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+x = [1,2,3,4,5,6]
+y = [1,4,9,16,25,36]
+plt.plot(x,y )
+plt.show()
